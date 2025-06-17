c2569c5ba29c92fd2e2701e59100e2872c769d69
France020800 <francescomarchini7@gmail.com>
2025-06-17 23:20:28 +0200

    exercise 1.2

---
+++ Lab1/main_1.2.py
@@ -0,0 +1,28 @@
+import os
+
+from models.ResidualMLP import ResidualMLP
+from comet_ml import start
+from comet_ml.integration.pytorch import log_model
+
+import utils
+import torch
+
+if __name__ == '__main__':
+    device = 'cuda' if torch.cuda.is_available else 'cpu'
+    hyper_params = {
+        'epochs': 10,
+        'lr': 0.0001,
+        'batch_size': 128,
+        'input_size': 28 * 28,
+        'width': 512,
+        'depth': 10,
+        'weight_decay': 5e-9,
+        'step_size': 4,
+        'gamma': 0.1,
+        'patience': 3,
+        'early': False
+    }
+
+    utils.set_seed(42)
+    model = ResidualMLP(hyper_params['input_size'], hyper_params['output_size'], hyper_params['width'], hyper_params['depth']).to(device)
+    utils.start_train(model, hyper_params, comet_project='MLP vs ResidualMLP', device=device)
